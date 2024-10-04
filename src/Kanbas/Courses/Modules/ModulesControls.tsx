587ccd948c24b3c5b7c7ad861b350f286acc61fc
Divit Pratap Singh <divitpratapsingh@Divits-MacBook-Pro.local>
2024-10-03 23:38:30 -0400

    a2 CSS and Bootstrap

--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -1,34 +1,94 @@
 import { FaPlus } from "react-icons/fa6";
 import GreenCheckmark from "./GreenCheckmark";
 import React from "react";
+
 export default function ModulesControls() {
   return (
     <div id="wd-modules-controls" className="text-nowrap">
-      <button id="wd-add-module-btn" className="btn btn-lg btn-danger me-1 float-end">
+      <button
+        id="wd-add-module-btn"
+        className="btn btn-lg btn-danger me-1 float-end"
+      >
         <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-        Module</button>
+        Module
+      </button>
       <div className="dropdown d-inline me-1 float-end">
-        <button id="wd-publish-all-btn" className="btn btn-lg btn-secondary dropdown-toggle"
-          type="button" data-bs-toggle="dropdown">
+        <button
+          id="wd-publish-all-btn"
+          className="btn btn-lg btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+        >
           <GreenCheckmark />
-          Publish All</button>
+          Publish All
+        </button>
         <ul className="dropdown-menu">
           <li>
-            <a id="wd-publish-all-modules-and-items-btn" className="dropdown-item" href="#">
+            <button
+              id="wd-publish-all-modules-and-items-btn"
+              className="dropdown-item"
+              onClick={() => {
+                console.log("Publish all modules and items");
+              }}
+            >
               <GreenCheckmark />
-              Publish all modules and items</a>
+              Publish all modules and items
+            </button>
           </li>
           <li>
-            <a id="wd-publish-modules-only-button" className="dropdown-item" href="#">
+            <button
+              id="wd-publish-modules-only-button"
+              className="dropdown-item"
+              onClick={() => {
+                console.log("Publish modules only");
+              }}
+            >
               <GreenCheckmark />
-              Publish modules only</a>
+              Publish modules only
+            </button>
           </li>
-          {/* Create two more items with IDs wd-unpublish-all-modules-and-items and
-              wd-unpublish-modules-only with labels Unpublish all modules and items
-              and Unpublish modules only */}
+          <li>
+            <button
+              id="wd-unpublish-all-modules-and-items"
+              className="dropdown-item"
+              onClick={() => {
+                console.log("Unpublish all modules and items");
+              }}
+            >
+              Unpublish all modules and items
+            </button>
+          </li>
+          <li>
+            <button
+              id="wd-unpublish-modules-only"
+              className="dropdown-item"
+              onClick={() => {
+                console.log("Unpublish modules only");
+              }}
+            >
+              Unpublish modules only
+            </button>
+          </li>
         </ul>
       </div>
-      {/* Implement the View Progress and Collapse All buttons with IDs wd-view-progress and wd-collapse-all */}
+      <button
+        id="wd-view-progress"
+        className="btn btn-lg btn-info me-1 float-end"
+        onClick={() => {
+          console.log("View progress");
+        }}
+      >
+        View Progress
+      </button>
+      <button
+        id="wd-collapse-all"
+        className="btn btn-lg btn-warning float-end"
+        onClick={() => {
+          console.log("Collapse all");
+        }}
+      >
+        Collapse All
+      </button>
     </div>
-);}
-
+  );
+}
--- src/Labs/Lab2/BootstrapNavigation.tsx
+++ src/Labs/Lab2/BootstrapNavigation.tsx
@@ -1,48 +1,44 @@
-import React from "react"
-export default function BootstrapNavigation(){
-  return(
-    <div id="wd-bs-navigation">
-        <div id="wd-css-navigating-with-tabs">
-    <h2>Tabs</h2>
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-          <a className="nav-link active" href="#">Active</a>
-      </li>
-      <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-      </li>
-      <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-      </li>
-      <li className="nav-item">
-          <a className="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
-  <div id="wd-css-navigating-with-cards">
-  <h2>
-    Cards
-  </h2>
-  <div className="card"
-       style={{ width: "18rem" }}>
-    <img src="images/stacked.jpg"
-         className="card-img-top" />
-    <div className="card-body">
-      <h5 className="card-title">
-          Stacking Starship
-      </h5>
-      <p className="card-text">
-        Stacking the most powerful rocket in history. Mars or bust!
-      </p>
-      <a href="#" className="btn btn-primary">
-        Boldly Go
-      </a>
-    </div>
-  </div>
-</div>
+import React from "react";
 
+export default function BootstrapNavigation() {
+  return (
+    <div id="wd-bs-navigation">
+      <div id="wd-css-navigating-with-tabs">
+        <h2>Tabs</h2>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <a className="nav-link active" href="#active">Active</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#link1">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#link2">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+      <div id="wd-css-navigating-with-cards">
+        <h2>Cards</h2>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="images/stacked.jpg"
+            className="card-img-top"
+            alt="A stacked rocket" // Add meaningful alt text
+          />
+          <div className="card-body">
+            <h5 className="card-title">Stacking Starship</h5>
+            <p className="card-text">
+              Stacking the most powerful rocket in history. Mars or bust!
+            </p>
+            <a href="#boldly-go" className="btn btn-primary">
+              Boldly Go
+            </a>
+          </div>
         </div>
-  
+      </div>
+    </div>
   );
-    
 }
