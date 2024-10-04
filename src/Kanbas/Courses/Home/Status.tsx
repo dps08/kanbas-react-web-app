840cddad800e5cfdad56dcd4b1534d4e776b1e05
Divit Pratap Singh <divitpratapsingh@Divits-MacBook-Pro.local>
2024-10-03 23:54:28 -0400

    a2 CSS and Bootstrap

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -3,25 +3,43 @@ import { FaCheckCircle } from "react-icons/fa";
 import { BiImport } from "react-icons/bi";
 import { LiaFileImportSolid } from "react-icons/lia";
 import React from "react";
-{/* Find more icons */}
+
 export default function CourseStatus() {
   return (
     <div id="wd-course-status" style={{ width: "300px" }}>
       <h2>Course Status</h2>
       <div className="d-flex">
         <div className="w-50 pe-1">
-          <button className="btn btn-lg btn-secondary w-100 text-nowrap ">
-            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish </button>
+          <button 
+            className="btn btn-lg btn-secondary w-100 text-nowrap" 
+            aria-label="Unpublish the course"
+          >
+            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish 
+          </button>
         </div>
         <div className="w-50">
-          <button className="btn btn-lg btn-success w-100">
-            <FaCheckCircle className="me-2 fs-5" /> Publish </button>
+          <button 
+            className="btn btn-lg btn-success w-100" 
+            aria-label="Publish the course"
+          >
+            <FaCheckCircle className="me-2 fs-5" /> Publish 
+          </button>
         </div>
-      </div><br />
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <BiImport className="me-2 fs-5" /> Import Existing Content </button>
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons </button>
-      {/* Complete the rest of the buttons */}
+      </div>
+      <br />
+      <button 
+        className="btn btn-lg btn-secondary w-100 mt-1 text-start" 
+        aria-label="Import existing content"
+      >
+        <BiImport className="me-2 fs-5" /> Import Existing Content 
+      </button>
+      <button 
+        className="btn btn-lg btn-secondary w-100 mt-1 text-start" 
+        aria-label="Import from Commons"
+      >
+        <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons 
+      </button>
+      {/* Add additional buttons here if necessary */}
     </div>
-);}
+  );
+}
--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -3,45 +3,86 @@ import { LiaBookSolid } from "react-icons/lia";
 import { FaRegCircleUser } from "react-icons/fa6";
 import React from "react";
 import { Link } from "react-router-dom";
+
 export default function KanbasNavigation() {
   return (
-    <div id="wd-kanbas-navigation" style={{ width: 120 }} 
-         className="list-group rounded-0 position-fixed
-         bottom-0 top-0 d-none d-md-block bg-black z-2">
-      <a id="wd-neu-link" target="_blank" 
+    <div 
+      id="wd-kanbas-navigation" 
+      style={{ width: 120 }} 
+      className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
+    >
+      <a 
+        id="wd-neu-link" 
+        target="_blank" 
+        rel="noopener noreferrer" // Added for security
         href="https://www.northeastern.edu/"
-        className="list-group-item bg-black border-0 text-center">
-        <img src="/images/NEU.png" width="75px" /></a>
-      <Link to="/Kanbas/Account" id="wd-account-link"
-        className="list-group-item text-center border-0 bg-black text-white">
+        className="list-group-item bg-black border-0 text-center"
+        aria-label="Northeastern University website"
+      >
+        <img src="/images/NEU.png" width="75px" alt="Northeastern University Logo" /> {/* Added alt text */}
+      </a>
+      
+      <Link 
+        to="/Kanbas/Account" 
+        id="wd-account-link"
+        className="list-group-item text-center border-0 bg-black text-white"
+        aria-label="Account"
+      >
         <FaRegCircleUser className="fs-1 text text-white" /><br />
-        Account </Link>
-      <Link to="/Kanbas/Dashboard" id="wd-dashboard-link"
-        className="list-group-item text-center border-0
-                   bg-white text-danger">
+        Account 
+      </Link>
+      
+      <Link 
+        to="/Kanbas/Dashboard" 
+        id="wd-dashboard-link"
+        className="list-group-item text-center border-0 bg-white text-danger"
+        aria-label="Dashboard"
+      >
         <AiOutlineDashboard className="fs-1 text-danger" /><br />
-        Dashboard </Link>
-      <Link to="/Kanbas/Dashboard" id="wd-course-link"
-        className="list-group-item text-white
-                   bg-black text-center border-0">
+        Dashboard 
+      </Link>
+      
+      <Link 
+        to="/Kanbas/Courses"
+        id="wd-course-link"
+        className="list-group-item text-white bg-black text-center border-0"
+        aria-label="Courses"
+      >
         <LiaBookSolid className="fs-1 text-danger" /><br />
-        Courses </Link>
-        <Link to="/Kanbas/Dashboard" id="wd-calendar-link"
-        className="list-group-item text-white
-                   bg-black text-center border-0">
+        Courses 
+      </Link>
+      
+      <Link 
+        to="/Kanbas/Calendar" 
+        id="wd-calendar-link"
+        className="list-group-item text-white bg-black text-center border-0"
+        aria-label="Calendar"
+      >
         <LiaBookSolid className="fs-1 text-danger" /><br />
-        Calender </Link>
-        <Link to="/Kanbas/Inbox" id="wd-inbox-link"
-        className="list-group-item text-white
-                   bg-black text-center border-0">
+        Calendar 
+      </Link>
+      
+      <Link 
+        to="/Kanbas/Inbox" 
+        id="wd-inbox-link"
+        className="list-group-item text-white bg-black text-center border-0"
+        aria-label="Inbox"
+      >
         <LiaBookSolid className="fs-1 text-danger" /><br />
-        Inbox </Link>
-        <Link to="/Labs" id="wd-labs-link"
-        className="list-group-item text-white
-                   bg-black text-center border-0">
+        Inbox 
+      </Link>
+      
+      <Link 
+        to="/Labs" 
+        id="wd-labs-link"
+        className="list-group-item text-white bg-black text-center border-0"
+        aria-label="Labs"
+      >
         <LiaBookSolid className="fs-1 text-danger" /><br />
-        Labs </Link>
+        Labs 
+      </Link>
     </div>
-    );
+  );
 }
-export{}
+
+export {};
