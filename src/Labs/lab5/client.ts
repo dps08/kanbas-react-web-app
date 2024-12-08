080ac93c94f9bb6f2cec1cb1dedaacb3fb29f778
dps08 <singh.divi@northeastern.edu>
2024-12-08 18:49:59 -0500

    assignment 6 react app

--- src/Labs/lab5/client.ts
+++ src/Labs/lab5/client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+const REMOTE_SERVER = "https://kanbas-node-server-app-6-0e365f0d0bc9.herokuapp.com";
 export const fetchWelcomeMessage = async () => {
   const response = await axios.get(`${REMOTE_SERVER}/lab5/welcome`);
   return response.data;
